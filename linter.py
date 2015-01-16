42f526e316a9cb6c8c105882a66abdb891f1f4a2
Jonas Gratz <j.gratz@epages.com>
2015-01-16 14:18:36 +0100

    use the new Epages6 plugin for configuring the vm and the tlec script

--- linter.py
+++ linter.py
@@ -3,22 +3,25 @@
 # Linter for SublimeLinter3, a code checking framework for Sublime Text 3
 #
 # Written by Jonas Gratz
-# Copyright (c) 2014 Jonas Gratz
+# Copyright (c) 2015 Jonas Gratz
 #
 # License: MIT
 #
 
 """This module exports the Tlec plugin class."""
 
+import sublime
 from SublimeLinter.lint import Linter, util
-
 
 class Tlec(Linter):
 
     """Provides an interface to tlec."""
 
+    def cmd(self):
+        return [self.executable_path, sublime.load_settings('Epages6.sublime-settings').get('path'), '--vm=' + self.view.settings().get('ep6_vm'), '--lint', '@'];
+
+    executable = 'perl'
     syntax = ('html', 'tle')
-    cmd = 'tlec @'
     regex = r'(?P<message>.+?) at line (?P<line>\d+)(, near (?P<near>.+?))?'
     error_stream = util.STREAM_BOTH
     tempfile_suffix = 'html'
